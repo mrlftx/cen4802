af810472a4882b14d8ef48a70b9d3a4f96512a8c
mrlftx <mrlftx@gmail.com>
2023-05-17 14:48:57 -0400

    Add files via upload

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,13 +1,24 @@
 package Fib;
-
+/**
+ * 
+ * @author Luiz Felipe Tafner
+ *
+ */
 public class Fibonacci {
-	
+	/**
+	 * This method recursively finds the nth number in the Fibonacci sequence, where n = x.
+	 * @param x The position in the Fibonacci seqeucne we want the number from.
+	 * @return The actual proper Fibonacci number that is in the x position in the sequence.
+	 */
 	public static int findFib(int x) {
 		if (x <= 1)
 			return x;
 		return findFib(x-1) + findFib(x-2);
 	}
-	
+	/**
+	 * This main method runs the findFib method once (with a default inout of 10) and prints out the number that is returned.
+	 * @param args The command line arguments (Unused).
+	 */
 	public static void main(String[] args) {
 		int holder = findFib(10);
 		System.out.println("The 10th fibonacci number is : " + holder);
